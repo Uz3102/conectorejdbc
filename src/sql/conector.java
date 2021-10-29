65e3301409a1ffb9e3451b7c640f809687d6fd59
oscar <oscaruceta.dam@ciudadescolarfp.es>
2021-10-29 11:59:17 +0200

    print

--- src/sql/conector.java
+++ src/sql/conector.java
@@ -10,12 +10,12 @@ public class conector {
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/pruebas","root", "7145");
 			
 			Statement stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery("select * from personas  ");
+			ResultSet rs = stmt.executeQuery("select * from personas ");
 			while (rs.next())
 				System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3) + " ");
 			con.close();
 		}catch (SQLException e) {
-			System.out.println(e);
+			System.out.println("hola");
 		}
 	}
 }
